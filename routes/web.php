60ed7239275d6640acf78a63fcca528875556147
gvaldovi <gvaldovi@gmail.com>
2021-01-25 19:59:04 -0600

    Crud con jetstream y liveware

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::resource('articulos','App\Http\Controllers\ArticuloController');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+//Con la ruta de arriba debe de estar autenticado o con esta nueva,
+//entra directo
+//Route::get('/dash','App\Http\Controllers\DashboardController@index');
+
+Route::get('/dash/crud/', function () {
+    return view('crud.index');
+});
+
+Route::get('/dash/crud/create', function () {
+    return view('crud.create');
+});
